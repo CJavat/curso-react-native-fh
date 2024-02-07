64fa3c7e663256f4e86fb6285a705ba09af4c0de
cjavat <cdpm98@hotmail.com>
2024-02-06 20:51:18 -0600

    SECCIÓN 2 - TERMINADO

---
+++ 01-reforzamiento/src/App.tsx
@@ -0,0 +1,17 @@
+import Funciones from "./typescript/Funciones";
+// import { ObjetosLiterales } from "./typescript/ObjetosLiterales";
+// import { TiposBasicos } from "./typescript/TiposBasicos";
+
+const App = () => {
+  return (
+    <div className="mt-2">
+      <h1>Introducción a TS-React</h1>
+      <hr />
+      {/* <TiposBasicos /> */}
+      {/* <ObjetosLiterales /> */}
+      <Funciones />
+    </div>
+  )
+}
+
+export default App;
---
+++ 01-reforzamiento/src/typescript/TiposBasicos.tsx
@@ -0,0 +1,12 @@
+export const TiposBasicos = () => {
+
+  const nombre: string = "Fernando";
+  const edad: number = 35;
+  const estaActivo: boolean = true;
+
+  return (
+    <>
+      <h3>TiposBasicos</h3>
+    </>
+  )
+}
