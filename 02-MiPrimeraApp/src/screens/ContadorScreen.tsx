3e6e9d6113fff9b91bc35b9e83dd9366c39b8dce
cjavat <cdpm98@hotmail.com>
2024-02-09 19:11:49 -0600

    SECCIÓN 4 - TERMINADO

--- 02-MiPrimeraApp/src/screens/ContadorScreen.tsx
+++ 02-MiPrimeraApp/src/screens/ContadorScreen.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { Fab } from '../components/Fab';
 
 export const ContadorScreen = () => {
   const [contador, setContador] = useState(10);
@@ -10,23 +11,25 @@ export const ContadorScreen = () => {
         Contador: { contador }
       </Text>
 
-      <TouchableOpacity
-        style={ styles.fabLocationBR }
-        onPress={ () => setContador( contador + 1 ) }
-      >
-        <View style={ styles.fab }>
-          <Text style={ styles.fabText }>+1</Text>
-        </View>
-      </TouchableOpacity>
+      <Fab 
+        title="+1"
+        onPress={ () => setContador(contador + 1) }
+      />
+      
+      <Fab 
+        title="-1"
+        position='bl'
+        onPress={ () => setContador(contador - 1) }
+      />
 
-      <TouchableOpacity
+      {/* <TouchableOpacity
         style={ styles.fabLocationBL }
         onPress={ () => setContador( contador - 1 ) }
       >
         <View style={ styles.fab }>
           <Text style={ styles.fabText }>-1</Text>
         </View>
-      </TouchableOpacity>
+      </TouchableOpacity> */}
 
     </View>
   )
@@ -40,29 +43,5 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 40
-  },
-  fabLocationBR: {
-    position: 'absolute',
-    bottom: 20,
-    right: 20,
-  },
-  fabLocationBL: {
-    position: 'absolute',
-    bottom: 20,
-    left: 20,
-  },
-  fab: {
-    backgroundColor: '#5856D6',
-    width: 60,
-    height: 60,
-    borderRadius: 100,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  fabText: {
-    color: 'white',
-    fontSize: 25,
-    fontWeight: 'bold',
   }
-
 });
