6b11a332bcd4791a4dfd5d89eed691dc9b95e16a
cjavat <cdpm98@hotmail.com>
2024-02-10 22:43:43 -0600

    Errores al instalar react-native-animated

--- 04-navegacionApp/src/screens/Pagina2Screen.tsx
+++ 04-navegacionApp/src/screens/Pagina2Screen.tsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigation } from '@react-navigation/native'
 import { Button, Text, View } from 'react-native'
 import { styles } from '../themes/app.theme'
-import { useNavigation } from '@react-navigation/native'
 
 export const Pagina2Screen = () => {
 
   const navigation = useNavigation();
 
+  useEffect(() => {
+    navigation.setOptions({
+      title: "Hola Mundo",
+      headerBackTitle: 'Atrás'
+    });
+  }, [])
+  
+
   return (
     <View style={ styles.globalMargin }>
-      <Text>Pagina2Screen</Text>
+      <Text style={ styles.title } >Pagina2Screen</Text>
 
       <Button
         title='Ir Página 3'
