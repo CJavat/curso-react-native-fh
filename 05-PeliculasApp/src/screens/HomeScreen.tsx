a62b48de564c4e969b624f84e9f61620c0f8adba
cjavat <cdpm98@hotmail.com>
2024-02-12 23:53:38 -0600

    SECCIÓN 10 / CAP 12 - EN CURSO

--- 05-PeliculasApp/src/screens/HomeScreen.tsx
+++ 05-PeliculasApp/src/screens/HomeScreen.tsx
@@ -1,21 +1,45 @@
-import React, { useEffect } from 'react'
-import { Text, View } from 'react-native'
-import movieDB from '../api/movieDB'
+import React from 'react'
+import { ActivityIndicator, Dimensions, FlatList, ScrollView, Text, View } from 'react-native'
+import Carousel from 'react-native-snap-carousel';
+import { useMovies } from '../hooks/useMovies'
+import { MoviePoster } from '../components/MoviePoster';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { HorizontalSlider } from '../components/HorizontalSlider';
+
+const { width: windowWidth } = Dimensions.get('window');
 
 export const HomeScreen = () => {
   
-  useEffect(() => {
-    movieDB.get('/now_playing')
-      .then( resp => {
-        console.log(resp.data);
-      })
-    
-  }, [])
+  const { peliculasEnCine, isLoading } = useMovies();
+  const { top } = useSafeAreaInsets();
   
+  if( isLoading ) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignContent: 'center' }}>
+        <ActivityIndicator color="red" size={ 100 } />
+      </View>
+    )
+  }
 
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <ScrollView>
+      <View style={{ marginTop: top + 20 }} >
+        {/* <MoviePoster movie={ peliculasEnCine[0] } /> */}
+
+        {/* Carousel Principal */}
+        <View style={{ height: 440 }}>
+          <Carousel 
+            data={ peliculasEnCine }
+            renderItem={ ({ item }) => <MoviePoster movie={ item } /> }
+            sliderWidth={ windowWidth }
+            itemWidth={ 300 }
+          />
+        </View>
+
+        {/* Películas Populares */}
+        <HorizontalSlider title='En Cine' movies={ peliculasEnCine } />
+      
+      </View>
+    </ScrollView>
   )
 }
