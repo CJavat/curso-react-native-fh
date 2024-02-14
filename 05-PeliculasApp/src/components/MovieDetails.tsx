5f4109a8c66a95c69c2b4a1e3c04a28b5abe0891
cjavat <cdpm98@hotmail.com>
2024-02-13 22:21:33 -0600

    SECCIÓN 10 - TERMINADO

--- 05-PeliculasApp/src/components/MovieDetails.tsx
+++ 05-PeliculasApp/src/components/MovieDetails.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { FlatList, Text, View } from 'react-native'
+import currencyFormatter from "currency-formatter";
 import Icon from "react-native-vector-icons/Ionicons";
 import { MovieFull } from '../interfaces/movie.Interface';
 import { Cast } from '../interfaces/credits.interface';
+import { CastItem } from './CastItem';
 
 interface Props {
   movieFull: MovieFull;
@@ -24,9 +26,46 @@ export const MovieDetails = ( { cast, movieFull }: Props ) => {
 
           <Text style={{ marginLeft: 5 }}> - { movieFull.genres.map( g => g.name ).join(', ') } </Text>
         </View>
+
+        {/* Historia */}
+        <Text style={{ fontSize: 23, marginTop: 10, fontWeight: 'bold' }}>
+          Historia
+        </Text>
+        
+        <Text style={{ fontSize: 16 }}>
+          { movieFull.overview }
+        </Text>
+        
+        {/* Presupuesto */}
+        <Text style={{ fontSize: 23, marginTop: 10, fontWeight: 'bold' }}>
+          Presupuesto
+        </Text>
+        
+        <Text style={{ fontSize: 18 }}>
+          { 
+            currencyFormatter.format( movieFull.budget, { code: 'USD' } )
+          }
+        </Text>
+        
+
       </View>
 
       {/* Casting */}
+      <View style={{ marginTop: 10, marginBottom: 100 }}>
+        <Text style={{ marginTop: 10, fontSize: 23, fontWeight: 'bold', marginHorizontal: 20 }}>
+          Actores
+        </Text>
+        <FlatList 
+          data={ cast }
+          keyExtractor={ ( item ) => item.id.toString() }
+          renderItem={ ({ item }) => <CastItem actor={ item } /> }
+          horizontal={ true }
+          showsHorizontalScrollIndicator={ false }
+          style={{ marginTop: 10, height: 70, }}
+        />
+        
+      </View>
+
     </>
   )
 }
--- 05-PeliculasApp/src/screens/DetailScreen.tsx
+++ 05-PeliculasApp/src/screens/DetailScreen.tsx
@@ -1,15 +1,17 @@
 import { StackScreenProps } from '@react-navigation/stack'
 import React from 'react'
-import { ActivityIndicator, Dimensions, Image, ScrollView, StyleSheet, Text, View } from 'react-native'
+import { ActivityIndicator, Dimensions, Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import { RootStackParams } from '../navigation/Navigation';
 import { useMovieDetails } from '../hooks/useMovieDetails';
 import { MovieDetails } from '../components/MovieDetails';
+import Icon from "react-native-vector-icons/Ionicons";
+
 
 interface Props extends StackScreenProps<RootStackParams, 'DetailScreen'> {}
 
 const screenHeight = Dimensions.get('screen').height
 
-export const DetailScreen = ( { route }: Props ) => {
+export const DetailScreen = ( { route, navigation }: Props ) => {
 
   const movie = route.params;
   const uri = `https://image.tmdb.org/t/p/w500${ movie.poster_path }`;
@@ -37,6 +39,17 @@ export const DetailScreen = ( { route }: Props ) => {
           ? <ActivityIndicator size={ 35 } color="gray" style={{ marginTop: 15 }} />
           : <MovieDetails movieFull={ movieFull! } cast={ cast } />
       }
+
+      {/* Botón para cerrar */}
+      <View style={ styles.backButton }>
+        <TouchableOpacity onPress={ () => navigation.pop() }>
+          <Icon 
+            color="white" 
+            name="arrow-back-outline" 
+            size={40}
+            />
+        </TouchableOpacity>
+      </View>
     </ScrollView>
   )
 }
@@ -80,5 +93,13 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     fontWeight: 'bold'
+  },
+  backButton: {
+    position: 'absolute',
+    top: 20,
+    left: 15,
+    zIndex: 1000,
+    elevation: 9,
+    color: 'white',
   }
 });
